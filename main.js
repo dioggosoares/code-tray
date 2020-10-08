bd35c78f09c88b3fd20b230755da445efc3e1238
Diogo <dioggo.soares@live.com>
2020-10-08 20:22:53 -0300

    Adicionando cross- spawn e build config

--- main.js
+++ main.js
@@ -1,14 +1,65 @@
-const { resolve } = require('path');
-const { app, Menu, Tray } = require('electron');
+const { resolve, basename } = require('path');
+const { app, Menu, Tray, dialog, MenuItem } = require('electron');
+const spawn = require('cross-spawn');
+const Store = require('electron-store');
+
+const schema = {
+    projects: {
+        type: 'string',
+    },
+};
+
+const store = new Store({ schema });
+// store.clear();
+
+let tray = null;
+
+function render(){
+
+    if(!tray.isDestroyed()){
+        tray.destroy();
+        tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
+    }
+
+    const storedProjects = store.get('projects');
+    const projects = storedProjects ? JSON.parse(storedProjects) : [];
+
+    const items = projects.map(project => ({
+        label: project.name,
+        click: () => {
+            spawn.sync('code', [project.dirPath], { stdio: 'inherit' });
+        },
+    }));
+    
+    const contextMenu = Menu.buildFromTemplate([
+        ...items,
+        {
+            type: 'separator',
+        },
+        ]);
+    
+    contextMenu.insert(0, new MenuItem({
+        label: 'Adicionar novo projeto...',
+        click: async () => {
+
+            const path = await dialog.showOpenDialog({ properties: ['openDirectory'] });
+            const dirPath = path.filePaths.join(' ');
+            const name = basename(dirPath);
+            
+            store.set('projects', JSON.stringify([...projects, {
+                dirPath,
+                name,
+            }]));
+
+            render();
+        },
+    }));
+
+    tray.setContextMenu(contextMenu);
+}
 
 app.on('ready', () => {
+    tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
 
-    const tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
-
-    const contextMenu = Menu.buildFromTemplate([
-        { label: 'Item1', type: 'radio', checked: true }
-    ]);
-
-    tray.setToolTip('Minha app desktop');
-    tray.setContextMenu(contextMenu);
+    render();
 });
